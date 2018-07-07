cdac3a523ac51a374d7f1865a733600d692af7aa
Ngoc Duy (Yui) Pham <duyp@cakesolutions.net>
2018-07-07 11:29:37 +0100

    Extend

--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -33,8 +33,7 @@ instance Extend ExactlyOne where
     (ExactlyOne a -> b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance ExactlyOne"
+  (<<=) f = ExactlyOne . f
 
 -- | Implement the @Extend@ instance for @List@.
 --
@@ -51,8 +50,8 @@ instance Extend List where
     (List a -> b)
     -> List a
     -> List b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance List"
+  f <<= a :. t = (f (a :. t)) :. (f <<= t)
+  f <<= Nil = Nil
 
 -- | Implement the @Extend@ instance for @Optional@.
 --
@@ -66,8 +65,7 @@ instance Extend Optional where
     (Optional a -> b)
     -> Optional a
     -> Optional b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Optional"
+  f <<= a = (f . Full) <$> a
 
 -- | Duplicate the functor using extension.
 --
@@ -86,5 +84,4 @@ cojoin ::
   Extend f =>
   f a
   -> f (f a)
-cojoin =
-  error "todo: Course.Extend#cojoin"
+cojoin = (<<=) id
